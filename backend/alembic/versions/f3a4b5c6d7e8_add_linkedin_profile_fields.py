70ae489c98fbaf6d706a8f95f53a9fa76958772a
Nick Sawinyh <nikita@sawinyh.ru>
2026-03-10 17:28:53 -0400

    Fix: chain linkedin_profile migration after twitter_user_id to resolve multiple heads

--- backend/alembic/versions/f3a4b5c6d7e8_add_linkedin_profile_fields.py
+++ backend/alembic/versions/f3a4b5c6d7e8_add_linkedin_profile_fields.py
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision: str = 'f3a4b5c6d7e8'
-down_revision: Union[str, None] = '97d166ac9db2'
+down_revision: Union[str, None] = 'a8b9c0d1e2f3'
 branch_labels = None
 depends_on = None
 
